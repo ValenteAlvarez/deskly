6295344675c68c0aac115a25fbd90c3a1b186fcb
ValenteAlvarez <valentealvarez1@gmail.com>
2026-08-16 16:57:03 -0600

    implement pagination and filtering for querying tickets

--- backend/app/routers/tickets.py
+++ backend/app/routers/tickets.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.models.ticket import Ticket
+from app.models.ticket import Priority, Ticket
 from app.schemas.ticket import TicketCreate, TicketRead
 
-router = APIRouter()
-@router.post('/', response_model=TicketRead)
+router = APIRouter(prefix='/api')
+
+@router.post('/tickets', response_model=TicketRead)
 async def create_ticket(payload: TicketCreate):
 	new_ticket = Ticket(
 		title=payload.title, 
@@ -25,3 +26,26 @@ async def create_ticket(payload: TicketCreate):
         updated_at=new_ticket.updated_at,
         comments=new_ticket.comments,
     )
+
+@router.get('/tickets', response_model=list[TicketRead])
+async def get_tickets(page: int = 0, priority: Priority | None = None ) -> list[TicketRead]:
+	limit = 5 # Para mostrar pagination mas facil
+	tickets = []
+	filter = {}
+	if priority is not None:
+		filter = Ticket.priority == priority
+	
+	async for ticket in Ticket.find(filter).skip(page * limit).limit(limit):
+		tickets.append(TicketRead(
+			id=str(ticket.id),
+			title=ticket.title,
+			description=ticket.description,
+			priority=ticket.priority,
+			state=ticket.state,
+			assigned_to=ticket.assigned_to,
+			comments=ticket.comments,
+			created_at=ticket.created_at,
+			updated_at=ticket.updated_at
+		))
+	print(tickets)
+	return tickets
